301b6d19a7e2e4715d6151038703006030af1d5c
pradnyabhukan <bhukanpradnya5@gmail.com>
2023-08-17 19:49:16 -0400

    removed images from nextconfig

--- next.config.js
+++ next.config.js
@@ -2,15 +2,7 @@
 const nextConfig = {
     experimental: {
         appDir: true,
-    },
-    // images: {
-    //     remotePatterns: [
-    //       {
-    //         protocol: 'https',
-    //         hostname: 'image.tmdb.org',
-    //       },
-    //     ],
-    //   },
+    }
 }
 
 module.exports = nextConfig
